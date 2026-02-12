0b864fbee1d24eaf28664afa512ef9918703c1e6
tomo-local <tomohiro.togashi@gmail.com>
2026-02-13 00:18:27 +0900

    feat: add question 001

---
+++ app/question_001/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+		fmt.Println("Hello");
+		fmt.Println(`123`);
+
+		var name string = "togashi"
+		var age int = 28
+
+		fmt.Printf("Name: %s, Age: %d", name, age)
+}
